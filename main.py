4bf5db60bc626e79d0a82081b4579c6e1b7df817
Vivian Brown <vivian@eff.org>
2019-01-05 21:06:50 -0500

    Scrape styles from list

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import http.cookiejar
 import urllib.request
 
+import time
+
 from lxml.cssselect import CSSSelector
 from lxml.html import fromstring
 
@@ -12,25 +14,44 @@ cj.load()
 opener = urllib.request.build_opener(urllib.request.HTTPCookieProcessor(cj))
 
 index_html = opener.open(ARTICLES_INDEX_URL).read()
-soup = fromstring(index_html)
+page = fromstring(index_html)
 
+# Get a list of articles to visit
 article_urls = []
-sel = CSSSelector("a.title")
-for article_link in sel(soup):
+for article_link in page.cssselect("a.title"):
     article_urls.append(article_link.get('href'))
 
-# titles_file = open('titles.txt', 'w')
-body_file = open('body.txt', 'w')
+class Style:
+    def __init__(self, name, selector, newline=False):
+        self.name = name
+        self.selector = selector
+        self.separator = "\n" if newline else ""
+        self.f = open(Path("corpus/" + name + ".txt"), "w")
+
+    def select_from_page(self, page):
+        for el in page.cssselect(self.selector):
+            if el.text:
+                self.f.write(el.text + self.separator)
+
+styles = [
+    Style("title", ".main-wrapper h1", True),
+    Style("subtitle", "h1 + h2"),
+    # Style("section_header", "#key-takeaway p"),
+    Style("section", "section > p"),
+    Style("key_takeaway_title", "#key-takeaway h3", True),
+    Style("key_takeaway_body", "#key-takeaway p"),
+    # Style("bullet_bold", "#key-takeaway p", True),
+    # Style("bullet_body", "#key-takeaway p"),
+]
 
 for url in article_urls:
     html = opener.open("https://www.forrester.com" + url).read()
-    soup = fromstring(html)
-    sel = CSSSelector("section > p")
-    for txt in sel(soup):
-        body_file.write(txt.text or "")
-    break
+    page = fromstring(html)
+    for style in styles:
+        style.select_from_page(page)
+    time.sleep(5)
 
-# titles_file.close()
-body_file.close()
-
+# Close files
+for style in styles:
+    style.f.close()
 
